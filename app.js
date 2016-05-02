328593dfb73f56d05588e64f4a2f85b788e06d4d
Zoran Bogicevic <zbogicevic@outlook.com>
2016-05-02 12:37:18 +0000

    Added catch all route and route :variable support

--- app.js
+++ app.js
@@ -11,9 +11,24 @@ app.get('/bye', function(req, res) {
 
 app.get('/dog', function(req, res) {
     res.send('Woof');
-})
+});
 
-// Cloud9 setup, no port hardcoding
+// introducing route variables
+app.get('/r/:post', function(req, res) {
+    res.send('This page is to show any entered post');
+});
+
+// multiple route variables
+app.get('/r/:post/comments/:id/:title', function(req, res) {
+    res.send('This page is to show comments on post, with its id and title');
+});
+
+// catch all route - order matters, keep it at the bottom
+app.get('*', function(req, res) {
+    res.send('Nothin\' to see here!');
+});
+
+// Cloud9 setup, no port or IP hardcoding
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log('Server started ====');
 });
